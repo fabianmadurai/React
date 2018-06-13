7a98b0539f2569b602a04a7584afd138c6f5b3e1
fabianmadurai <fabianmadurai@gmail.com>
2018-06-13 23:08:11 +0200

    Version 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -15,16 +16,24 @@ class App extends Component {
                        selectedVideo:null
                 };
 //in our constructor we get a default search for Man united.
-        YTSearch({key:API_KEY,term:'World Cup 2018'},videos=>{
-            this.setState({videos:videos,
-                            selectedVideo :videos[0]
-            });
-       });
+       this.videoSearch('Zidane');
     }
+
+        videoSearch(term){
+            YTSearch({key:API_KEY,term:term},videos=>{
+                this.setState({videos:videos,
+                                selectedVideo :videos[0]
+                });
+           });
+        };
+
+
     render(){
+
+        const videoSearch = _.debounce(term=>{this.videoSearch(term)},300)
         return (
                     <div>
-                    <SearchBar /> 
+                    <SearchBar onSearchTermChange={videoSearch}/> 
                     <VideoDetail video={this.state.selectedVideo}/>  
                     <VideoList 
                     onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
